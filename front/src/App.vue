96b3def54cdb7befee027f0893fcaa960c89f267
akiryusei <1559469724@qq.com>
2022-08-26 11:31:13 +0800

    补充

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,7 +7,7 @@
       学生管理系统
     </h1>
     <el-button type="primary" @click = "getStudent">获取学生信息</el-button>
-    <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+    <el-button type="warning" @click="dialogVisible = true">添加学生</el-button>
 
 <el-dialog
   title="提示"
@@ -27,6 +27,40 @@
   <div><span>数学</span><input v-model.number="newStudent.math"/></div>
   <div><span>英语</span><input v-model.number="newStudent.eng"/></div>
 </el-dialog>
+<el-button type="info" @click="dialogVisiblelogin = true">login</el-button>
+
+<el-dialog
+  title="登录"
+  :visible.sync="dialogVisiblelogin"
+  width="30%"
+  :before-close="handleClose">
+  <span>用户名</span><input type="text" v-model="user_for_login.username">
+  <!-- v-model绑定 -->
+  <br>
+  <span>密码</span><input type="password" v-model="user_for_login.password">
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisiblelogin = false">取 消</el-button>
+    <el-button type="primary" @click="login">登录</el-button>
+  </span>
+</el-dialog>
+<el-button type="info" @click="dialogVisibleregister = true">register</el-button>
+<!-- type颜色 -->
+<el-dialog
+  title="注册"
+  :visible.sync="dialogVisibleregister"
+  width="30%"
+  :before-close="handleClose">
+  <span>用户名</span><input type="text" v-model="user_for_register.username">
+  <!-- v-model绑定 -->
+  <br>
+  <span>密码</span><input type="password" v-model="user_for_register.password">
+  <br>
+   <span>确认密码</span><input type="password" v-model="user_for_register.confirmPassword">
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisibleregister = false">取 消</el-button>
+    <el-button type="primary" @click="register">注册</el-button>
+  </span>
+</el-dialog>
   </caption>
   <thead>
     <tr>
@@ -37,12 +71,19 @@
       <th scope="col">数学</th>
       <th scope="col">英语</th>
       <th scope="col">操作</th>
+      <th></th>
     </tr>
   </thead>
   <tbody>
-    <Student v-for="stu in students" :key="stu.id" :student="stu"></Student>
+    <Student v-for="stu in students_for_page" :key="stu.id" :student="stu"></Student>
   </tbody>
 </table>
+<div class = "text-center">
+  <el-button-group>
+  <el-button type="primary" icon="el-icon-arrow-left" @click="last_page">上一页</el-button>
+  <el-button type="primary" @click="next_page">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+  </el-button-group>
+</div>
     </div>
   </div>
 </template>
@@ -57,7 +98,19 @@ export default {
   },
   data() {
     return{
+      user_for_login:{
+        username:"",
+        password:""
+      },
+      user_for_register:{
+        username:"",
+        password:"",
+        confirmPassword:""
+      },
+      page :1,
       dialogVisible: false,
+      dialogVisiblelogin: false,
+      dialogVisibleregister: false,
       students:[],
       newStudent:{
         number:"",
@@ -71,13 +124,18 @@ export default {
   },
   methods:{
     getStudent(){
-      axios({
+      if(sessionStorage.getItem("islogined")=="true"){
+        axios({
         url:"http://localhost:8080/students",
         method:'GET',
-      }).then(res=>{
-        console.log(res.data);
-        this.students = res.data;
-      })
+        }).then(res=>{
+          console.log(res.data);
+          this.students = res.data;
+        })
+      }else{
+        this.$alert("请先登录")
+      }
+      
     },
     // 对话框
     handleClose(done) {
@@ -94,8 +152,50 @@ export default {
         data:this.newStudent
       })
       this.dialogVisible = false
+    },
+    next_page(){
+      this.page+=1;
+    },
+    last_page(){
+      this.page-=1;
+    },
+    login(){
+      axios({
+        url:"http://localhost:8080/login",
+        method:'POST',
+        data:this.user_for_login
+      }).then(res=>{
+        console.log(res.data)
+        if(res.data == "1"){
+          sessionStorage.setItem("islogined","true");
+          //sessionStorage.clear()清除
+          //sessionStorage.getItem("")//拿取数据
+          //存的key value
+        }else{
+          alert("用户名或密码错误")
+        }
+        // localStorage.setItem("login",res.data)
+      })
+      this.dialogVisiblelogin = false
+      //登录状态的保持才是重点
+    },
+    register(){
+      // 注册按钮绑定事件
+      axios({
+        url:"http://localhost:8080/register",
+        method:"POST",
+        data:this.user_for_register
+      })
+      this.dialogVisibleregister = false;
+      this.$alert("注册成功")
     }
   },
+  computed:{
+    students_for_page(){
+      return this.students.slice(this.page*5-5,this.page*5)
+      //数组切片
+    }
+  }
 }
 </script>
 
--- front/src/components/Student.vue
+++ front/src/components/Student.vue
@@ -10,6 +10,8 @@
       <td v-show="!is_edit">{{student.eng}}</td>
       <td v-show="!is_edit">
         <el-button type = "primary" @click="modify">修改</el-button>
+      </td>
+      <td v-show="!is_edit">
         <el-button type = "danger" @click="deleteStudent">删除</el-button>
       </td>
       <td v-show="is_edit"><input class = "w-50" type="text" v-model = "student.number"/></td>
@@ -20,6 +22,8 @@
       <td v-show="is_edit"><input class = "w-50" type="text" v-model.number = "student.eng"/></td>
       <td v-show="is_edit">
         <el-button type = "primary" @click="save">保存</el-button>
+      </td>
+      <td v-show="is_edit">
         <el-button type = "danger">删除</el-button>
       </td>
     </tr>
@@ -51,7 +55,9 @@ export default {
                 url:"http://localhost:8080/delete",
                 method:"POST",
                 data:this.student
+                // 删除后需要自动刷新页面
             })
+            location.reload();
         }
     }
 }
